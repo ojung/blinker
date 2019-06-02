c8dbda7c058bb60711428ed8d25e0ad2de597b11
Oskar Jung <oskar@mbr-targeting.com>
2019-06-02 22:32:54 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::process::Command;
+
+extern crate logwatcher;
+use logwatcher::LogWatcher;
+
+fn main() -> std::io::Result<()> {
+    let mut log_watcher = LogWatcher::register("/var/log/pihole.log".to_string()).unwrap();
+
+    log_watcher.watch(|line: String| {
+        if line.contains("gravity") {
+            println!("blocked: {}", line);
+            blink(1)
+        } else {
+            blink(0)
+        }
+    });
+
+    Ok(())
+}
+
+fn blink(led_id: i8) {
+    let arg = format!("echo 1 > /sys/class/leds/led{}/brightness; sleep 0.001; echo 0 > /sys/class/leds/led{}/brightness;", led_id, led_id);
+    Command::new("bash")
+        .arg("-c")
+        .arg(arg)
+        .output()
+        .expect("failed change led value");
+}
